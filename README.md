4146c9f2f65758abf1ee753e5999247360f3d77c
Hailton <nascimento.haiton@gmail.com>
2022-01-18 09:52:25 -0300

    Criando Projeto do Conversor de Moeda

---
+++ README.md
@@ -0,0 +1,71 @@
+# Conversor de Moeda
+
+Este projeto é parte da Imersão Dev, realizada pela [Alura](https://www.alura.com.br/) em setembro de 2021.
+
+Durante a aula, utilizamos JavaScript para converter valores de dólar para real e mostrar o resultado na tela. 
+
+## Objetivo
+
+- Criar um aplicação que consulte a ultima cotação da moeda e faça a conversar para reais.
+
+<VIDEO   src="img/video.mp4" autoplay="true">
+
+# Índice
+# terra
+
+<p>
+    <ul>
+        <li><a href="#sobre">Sobre</a></li>
+    	<li><a href="#tecnologias">Tecnologias utilizadas</a></li>
+	</ul>
+</p>
+
+<h2 id="sobre">
+    ℹ️ Sobre
+</h2>
+
+O projeto é um conversor de moedas desenvolvido em HTML, CSS e Javascript puro, além de consumir uma API de cotação de moedas (<a href="https://docs.awesomeapi.com.br/api-de-moedas">AwesomeAPI</a>) com o intuito de praticar essas competências.
+
+---
+
+<h2 id="tecnologias">
+    🚀 Tecnologias utilizadas
+</h2>
+
+Foram utilizadas as seguintes tecnologias:
+
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Javascript</li>
+</ul>
+
+---
+
+
+
+
+### Contato
+
+- [Solution on my Github profile](https://github.com/nathannieg/frontendmentor-3-column-preview)
+- [Live site](https://3column-card-ng.netlify.app/)
+
+
+- Github - [Hailton Nascimento](https://github.com/Hailton-Nascimento)
+- LinkedIn - [Hailton Nascimento](https://linkedin.com/in/hailton-nascimento)
+
+
+
+
+
+
+
+[![LinkedIn][linkedin-shield]][linkedin-url]
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+
+
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/hailton-nascimento
+
---
+++ js/ConversorMoeda.js
@@ -0,0 +1,77 @@
+var valorDaMoedaSelcionada;
+var data;
+
+var url = `https://economia.awesomeapi.com.br/all`;
+var request = new XMLHttpRequest();
+var moedaSelecionada = '';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+
+});
+
+
+// Or with jQuery
+
+document.querySelector("input").addEventListener("change", function() {
+    resultadoConversao(event)
+});
+
+
+
+request.open('GET', url);
+request.onload = function() {
+    const moedas = JSON.parse(request.responseText);
+
+    let selectHtml = `<option value="" disabled selected>Selecione a moeda</option>`;
+
+    for (let moeda in moedas) {
+
+        selectHtml += `<option value="${moeda}">${moedas[moeda].name}</option>`;
+
+    }
+
+    document.querySelector("#moedas").innerHTML = selectHtml;
+
+}
+
+request.send();
+
+function getCotacao() {
+    var select = document.querySelector("#moedas");
+    var moedaSelecionada = select.options[select.selectedIndex].value;
+    const resposta = JSON.parse(request.responseText);
+    valorDaMoedaSelcionada = parseFloat(resposta[moedaSelecionada].bid);
+
+    data = new
+    Date(resposta[moedaSelecionada].create_date);
+
+    document.getElementById("valorCotacao").innerHTML = `Cotação do(a) ${resposta[moedaSelecionada].name.substr(0, resposta[moedaSelecionada].name.indexOf("/"))} <br>no dia ${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} as ${data.getHours()}:${data.getMinutes()} <br> <span class="valor">é R$ ${valorDaMoedaSelcionada.toFixed(2)} </span>`
+
+
+
+    if (resposta[moedaSelecionada].varBid < 0) {
+        document.getElementById("valorCotacao").classList.add("vermelho")
+        document.getElementById("valorCotacao").classList.remove("verde")
+    } else {
+        document.getElementById("valorCotacao").classList.add("verde");
+        document.getElementById("valorCotacao").classList.remove("vermelho")
+    }
+    resultadoConversao();
+}
+
+var valorEmDolarTexto = document.querySelector('#entradaValor');
+
+
+
+function resultadoConversao() {
+    // this.preventDefault();
+
+    let valorEmDolarNumero = parseFloat(valorEmDolarTexto.value);
+
+    let valorEmReal = valorEmDolarNumero * valorDaMoedaSelcionada;
+
+    let valorEmRealDecimal = valorEmReal.toFixed(2);
+
+    document.getElementById("valorConvertido").innerHTML = `R$ ${valorEmRealDecimal}`
+}
