ffce0278f125c5e5ed231acdb018b0499b0887cb
Hailton <nascimento.haiton@gmail.com>
2022-03-16 15:28:26 -0300

    Refatorando codigo

--- js/ConversorMoeda.js
+++ js/ConversorMoeda.js
@@ -1,77 +1,120 @@
-var valorDaMoedaSelcionada;
-var data;
-
-var url = `https://economia.awesomeapi.com.br/all`;
-var request = new XMLHttpRequest();
-var moedaSelecionada = '';
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-
-});
+const url = `https://economia.awesomeapi.com.br/all`;
+const selectMoedaElement = document.querySelector('#moedas');
+const inputQtdMoedaParaConversao = document.querySelector('#entradaValor');
+const loaderContainer = document.querySelector('.loader');
+const cotacaoConteiner = document.querySelector('.cotacao-container');
 
 
-// Or with jQuery
-
-document.querySelector("input").addEventListener("change", function() {
-    resultadoConversao(event)
-});
+let data;
+let moedaSelected = 'USD';
 
 
+const getCotacao = async() => {
+    const response = await fetch(url);
+    data = await response.json();
+    return data;
+};
 
-request.open('GET', url);
-request.onload = function() {
-    const moedas = JSON.parse(request.responseText);
-
-    let selectHtml = `<option value="" disabled selected>Selecione a moeda</option>`;
-
-    for (let moeda in moedas) {
-
-        selectHtml += `<option value="${moeda}">${moedas[moeda].name}</option>`;
-
+const generataMoedasTemplate = cotacoes => {
+    let templateOptions = "";
+    for (cotacao in cotacoes) {
+        const { code, name } = cotacoes[cotacao];
+        templateOptions += `<option value="${code}">${name}</option>`;
     }
+    return templateOptions;
+};
 
-    document.querySelector("#moedas").innerHTML = selectHtml;
+const insertOpitionsIntoSelection = async() => {
+    const cotacoes = await getCotacao();
+    const moedasTemplate = generataMoedasTemplate(cotacoes);
+    selectMoedaElement.innerHTML += moedasTemplate;
+};
 
+const removeLoader = () => {
+    setTimeout(() => {
+        loaderContainer.classList.remove("show");
+        setTimeout(() => {
+            handleCotacaoMoedaSelected(objetoMoedaSelected);
+            cotacaoConteiner.style.display = "flex"
+        }, 200);
+    }, 500);
+};
+
+const showLoader = () => {
+    cotacaoConteiner.style.display = "none";
+    loaderContainer.classList.add("show");
+    removeLoader();
+};
+const zeroFix = (valor) => valor < 10 ? `0${valor}` : valor;
+
+const criarData = (create_date) => {
+    const data = new Date(create_date);
+    const dia = zeroFix(data.getDate());
+    const mes = zeroFix(data.getMonth() + 1);
+    const hora = zeroFix(data.getHours());
+    const minuto = zeroFix(data.getMinutes());
+
+    return dataFormatted = `${dia}/${mes}/${data.getFullYear()} ÀS ${hora}:${minuto}`;
 }
 
-request.send();
+const setTencenciaDeVariacao = (varBid) => {
+    if (varBid < 0) {
+        document.querySelector(".valor-container").classList.add("dow")
+        document.querySelector(".valor-container").classList.remove("up")
 
-function getCotacao() {
-    var select = document.querySelector("#moedas");
-    var moedaSelecionada = select.options[select.selectedIndex].value;
-    const resposta = JSON.parse(request.responseText);
-    valorDaMoedaSelcionada = parseFloat(resposta[moedaSelecionada].bid);
-
-    data = new
-    Date(resposta[moedaSelecionada].create_date);
-
-    document.getElementById("valorCotacao").innerHTML = `Cotação do(a) ${resposta[moedaSelecionada].name.substr(0, resposta[moedaSelecionada].name.indexOf("/"))} no dia ${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} as ${data.getHours()}:${data.getMinutes()} <br> <span class="valor">é R$ ${valorDaMoedaSelcionada.toFixed(2)} </span>`
-
-
-
-    if (resposta[moedaSelecionada].varBid < 0) {
-        document.getElementById("valorCotacao").classList.add("vermelho")
-        document.getElementById("valorCotacao").classList.remove("verde")
     } else {
-        document.getElementById("valorCotacao").classList.add("verde");
-        document.getElementById("valorCotacao").classList.remove("vermelho")
+        document.querySelector(".valor-container").classList.add("up");
+        document.querySelector(".valor-container").classList.remove("dow")
     }
-    resultadoConversao();
+};
+
+
+const handleCotacaoMoedaSelected = ({ code, bid, create_date, name, varBid }) => {
+    const moeda = name.substr(0, name.indexOf("/"));
+    const valorDaMoedaSelcionada = parseFloat(bid);
+    const ultimaCotação = criarData(create_date);
+    setTencenciaDeVariacao(varBid);
+    const codeMoeda = code;
+    const cotacaoInfoContainerElemento =
+        document.querySelector('.cotacao-container');
+    const [elementCode, elementMoeda, elementInfoCotacao, elementValorCotacao] = ['code', 'moeda', 'info-cotacao', 'valorCotacao'].
+    map((classe) => cotacaoInfoContainerElemento.querySelector(`.${classe}`));
+    elementCode.innerHTML = `${codeMoeda}`;
+    elementMoeda.innerHTML = `${moeda} | `;
+    elementInfoCotacao.innerHTML = `Cotação na Data de: ${ultimaCotação}`
+    elementValorCotacao.innerHTML = `R$: ${(valorDaMoedaSelcionada).toFixed(2)}`
+
+};
+
+const stardedInput = () => {
+    setTimeout(() => {
+        const optionDolarAmericano =
+            selectMoedaElement.querySelector(`[value="${moedaSelected}"]`);
+        optionDolarAmericano.selected = true;
+        handleCotacaoMoedaSelected(data[moedaSelected]);
+    }, 500)
+
 }
 
-var valorEmDolarTexto = document.querySelector('#entradaValor');
+const setCotacao = () => {
+    moedaSelected = event.target.value;
+    objetoMoedaSelected = data[moedaSelected];
+    showLoader();
+    getConversao()
+};
 
+const handleConversao = (valorEmReal) => {
+    document.getElementById("valorConvertido").innerHTML = `R$ ${valorEmReal}`
+};
 
+const getConversao = () => {
+    const valorDaMoedaSelcionada = data[moedaSelected].bid;
+    const valorEmDolarNumero = parseFloat(inputQtdMoedaParaConversao.value);
+    const valorEmReal = (valorEmDolarNumero * valorDaMoedaSelcionada).toFixed(2);
+    handleConversao(valorEmReal)
+};
 
-function resultadoConversao() {
-    // this.preventDefault();
-
-    let valorEmDolarNumero = parseFloat(valorEmDolarTexto.value);
-
-    let valorEmReal = valorEmDolarNumero * valorDaMoedaSelcionada;
-
-    let valorEmRealDecimal = valorEmReal.toFixed(2);
-
-    document.getElementById("valorConvertido").innerHTML = `R$ ${valorEmRealDecimal}`
-}
+insertOpitionsIntoSelection();
+selectMoedaElement.addEventListener('change', setCotacao);
+inputQtdMoedaParaConversao.addEventListener('change', getConversao);
+window.addEventListener("load", stardedInput);
